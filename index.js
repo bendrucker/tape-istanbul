5c94f9c2ffe8125f4fa9bfec86d02e534ef71060
Ben Drucker <bvdrucker@gmail.com>
2016-07-03 18:36:23 -0700

    Wait on the coverage file to be written before ending stream

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 'use strict'
 
 const fs = require('fs')
+const pumpify = require('pumpify')
+const promiseify = require('pify')
+const through = require('through2')
 const parse = require('./parse')
 
 module.exports = tapeIstanbul
 
 function tapeIstanbul (output) {
-  return parse().on('coverage', function (coverage) {
-    fs.writeFile(output || 'coverage.json', JSON.stringify(coverage, null, 2))
+  // Create a pumpify pipeline populated in the scope below
+  const pause = pumpify()
+
+  // Use a promise to track the write of the coverage file
+  // Only end the stream when it resolves
+  // Promises are a decent way to accomplish this without much code
+  const p = new Promise(function (resolve, reject) {
+    pause.setPipeline(
+      parse().on('coverage', writeFile),
+      through(passThrough, flush)
+    )
+
+    function writeFile (coverage) {
+      promiseify(fs.writeFile)(output || 'coverage.json', JSON.stringify(coverage, null, 2))
+        .then(resolve)
+        .catch(reject)
+    }
   })
+
+  return pause
+
+  function flush (callback) {
+    p.then(callback).catch((err) => callback(err))
+  }
+}
+
+function passThrough (chunk, enc, callback) {
+  callback(null, chunk)
 }
