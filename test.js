326319baa9c0d848c99f96e2d81c4bd369e810f5
Todd Kennedy <toddself@users.noreply.github.com>
2017-05-15 16:42:52 -0700

    emit end and error events (#13)

--- test.js
+++ test.js
@@ -12,14 +12,16 @@ const tapeIstanbul = require('./')
 const tapOutput = fs.readFileSync('fixture-output.txt', 'utf8')
 
 test('api', function (t) {
-  t.plan(3)
+  t.plan(4)
 
   browserify()
     .add(path.resolve(__dirname, 'fixture-test.js'))
     .plugin(path.resolve(__dirname, 'plugin.js'))
     .bundle()
     .pipe(node())
-    .pipe(tapeIstanbul())
+    .pipe(tapeIstanbul().on('end', function () {
+      t.ok(true, 'end was emitted')
+    }))
     .pipe(concat(function (output) {
       t.equal(output.toString(), tapOutput, 'passes tap output through')
       const coverageFile = fs.readFileSync('coverage.json')
